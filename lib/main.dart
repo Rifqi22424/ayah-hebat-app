ee110975fb28d2d7678414bad17d503eab50c274
rifqi2242 <rifqimuzakki45@gmail.com>
2024-03-20 14:40:06 +0700

    change server to deploy server

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'src/my_app.dart';
 
-// const serverPath = "https://backend.ayahhebat.mangcoding.com";
-const serverPath = "http://192.168.1.23:3000";
+const serverPath = "https://backend.ayahhebat.mangcoding.com";
+// const serverPath = "http://192.168.1.23:3000";
 
 void main() {
   runApp(const MyApp());
